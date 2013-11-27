9f5e6c02ba77848e3d6d51558e0dd8af72d19c4a
Sam Popowich <spopowich@gmail.com>
2013-11-27 13:18:52 -0700

    Add logic for proble records.

--- summary_holdings.rb
+++ summary_holdings.rb
@@ -9,14 +9,18 @@ class SummaryHoldings
   def statement(object_id)
     if data[object_id] then
       summaryHoldings = data[object_id][:summary_holdings]
-      years = summaryHoldings.scan(/\d{4}/)
-      statement = String.new("")
-      (statement = summaryHoldings.split("-")[1] if summaryHoldings.split("-")[1] !~ /\d{4}/) unless summaryHoldings.split("-")[1].nil?
-      return "#{years[0]}".strip if (years.size==1 and statement.include?("only"))
-      return "#{years[0]}- #{statement}".strip if (years.size==1 and !statement.include?("only"))
-      return "#{years[0]}-#{years[1]}".strip if years.size==2
-      return "#{years[0]}-#{years[1]}, #{years[2]}-".strip if years.size==3
-      return "#{years[0]}-#{years[1]}, #{years[2]}-#{years[3]}".strip if years.size==4
+      if summaryHoldings == "-PROBLEM RECORD-"
+        return ""
+      else
+        years = summaryHoldings.scan(/\d{4}/)
+        statement = String.new("")
+        (statement = summaryHoldings.split("-")[1] if summaryHoldings.split("-")[1] !~ /\d{4}/) unless summaryHoldings.split("-")[1].nil?
+        return "#{years[0]}".strip if (years.size==1 and statement.include?("only"))
+        return "#{years[0]}- #{statement}".strip if (years.size==1 and !statement.include?("only"))
+        return "#{years[0]}-#{years[1]}".strip if years.size==2
+        return "#{years[0]}-#{years[1]}, #{years[2]}-".strip if years.size==3
+        return "#{years[0]}-#{years[1]}, #{years[2]}-#{years[3]}".strip if years.size==4
+      end
     else
       return "Not found in summary holdings file"
     end
